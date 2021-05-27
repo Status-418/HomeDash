299a2d90b563655ce986b028d06511c8185350e3
Guido Denzler <guido@denzler.me>
2021-05-27 09:13:49 +0100

    updates

--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'dashboard/index2.html')
+    from datetime import datetime
+    from .libs import test
+
+    context = dict()
+    
+    time = dict()
+    time['time'] = datetime.now().strftime("%H:%M")
+    time['date'] = datetime.now().strftime('%d/%m/%Y')
+    context['time'] = time
+
+    status = dict()
+    status['pihole'] = test.check_pihole()
+    status['cups'] = test.check_cups()
+    context['status'] = status
+
+    return render(request, 'dashboard/index2.html', context=context)
